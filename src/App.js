c7155f0033439ccae8cdd769f624ce73480ce20c
ykcsoftwarer <yusufkucukcopur@gmail.com>
2022-10-29 09:24:11 +0300

    components created

--- src/App.js
+++ src/App.js
@@ -2,25 +2,40 @@ import './App.css';
 import Button from './components/Button/Button';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
-import data from './assests/data';
-import {useState} from 'react';
+import data from './assests/data'
+
+
+// import Button from './components/Button/Button';
+// import Header from './components/Header/Header';
+// import Main from './components/Main/Main';
+// import data from './assests/data';
+// import {useState} from 'react';
 
 function App() {
-const [page, setPage] = useState(1);
-const userPerPage = 5;      // her sayfada 5 adet resim olsun
+// const [page, setPage] = useState(1);
+// const userPerPage = 5;      // her sayfada 5 adet resim olsun
 
-const indexOffLastUser = page * userPerPage;
-const indexOffFirstUser = indexOffLastUser - userPerPage;
+// const indexOffLastUser = page * userPerPage;
+// const indexOffFirstUser = indexOffLastUser - userPerPage;
 
-const currentUsers = data.slice(indexOffFirstUser, indexOffLastUser);
+// const currentUsers = data.slice(indexOffFirstUser, indexOffLastUser);
 
   return (
     <div className="App">
-      <Header from={indexOffFirstUser + 1} to={indexOffFirstUser + currentUsers.length}/>
+      {/* <Header from={indexOffFirstUser + 1} to={indexOffFirstUser + currentUsers.length}/>
       {currentUsers.map((item) => <Main key={item.id}{...item}/>)}
-      <Button total={Math.ceil(data.length / userPerPage)} page={page} setPage={setPage}/>
+      <Button total={Math.ceil(data.length / userPerPage)} page={page} setPage={setPage}/> */}
+
+      <Header/>
+      <Main/>
+      <Button/>
+
+
+
+
     </div>
   );
 }
+
 
 export default App;
