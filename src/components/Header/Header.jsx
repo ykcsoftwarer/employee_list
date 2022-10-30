c73de66b9b4d8130629b171f74798d778f28be12
ykcsoftwarer <yusufkucukcopur@gmail.com>
2022-10-30 11:58:34 +0300

    project added style code

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,10 @@ import './Header.css';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header' >
+        <h1 className="header-h1">EMPLOYEE LISTS</h1>
+        <h4 className="header-h4">{'Employes to'}</h4>
+    </div>
   )
 }
 
